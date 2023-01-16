9209782408544a44163af5be810fb15fc7328bc3
Lukas Schulte <lukas@promise.xyz>
2023-01-15 16:49:40 -0800

    Added error message to parser

--- datafusion/sql/src/parser.rs
+++ datafusion/sql/src/parser.rs
@@ -1064,6 +1064,11 @@ impl<'a> DFParser<'a> {
 
         self.parser.expect_keyword(Keyword::LOCATION)?;
         let location = self.parser.parse_literal_string()?;
+        if !Path::exists(Path::new(&location)) {
+            return Err(ParserError::ParserError(format!(
+                "Missing external file '{location}'"
+            )));
+        }
         let location2 = location.to_owned();
         let file_type2 = file_type.to_owned();
         let create = CreateExternalTable {
