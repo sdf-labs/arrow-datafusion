5edfc828f02e1e5075827be8f6724a7a11c78b76
lbroot <l0broot@gmail.com>
2023-10-27 20:28:36 +0800

    regexp_replace

--- extension/functions/src/postgres.rs
+++ extension/functions/src/postgres.rs
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use arrow::array::{ArrayRef, Int64Array, BooleanArray};
+use arrow::array::{ArrayRef, Int64Array, BooleanArray, StringArray};
 use arrow::datatypes::DataType;
 use datafusion::error::Result;
 use datafusion::logical_expr::Volatility;
@@ -150,12 +150,90 @@ impl ScalarFunctionDef for RegexpLikeFunction {
     }    
 }
 
+#[derive(Debug)]
+pub struct RegexpReplaceFunction;
+
+impl ScalarFunctionDef for RegexpReplaceFunction {
+    fn name(&self) -> &str {
+        "regexp_replace"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::one_of(
+            vec![
+                TypeSignature::Exact(vec![DataType::Utf8, DataType::Utf8, DataType::Utf8, DataType::Int64, DataType::Int64]),
+                TypeSignature::Exact(vec![DataType::Utf8, DataType::Utf8, DataType::Utf8, DataType::Int64, DataType::Int64, DataType::Utf8])
+            ],
+            Volatility::Immutable
+        )        
+    }
+    
+    fn return_type(&self) -> ReturnTypeFunction {
+        let return_type = Arc::new(DataType::Utf8);
+        Arc::new(move |_| Ok(return_type.clone()))
+    }
+
+    fn execute(&self, args: &[ArrayRef]) -> Result<ArrayRef> {
+        assert!(args.len() >= 5);
+    
+        let input = as_string_array(&args[0]).expect("cast failed");
+        let pattern = as_string_array(&args[1]).expect("cast failed");
+        let replacement = as_string_array(&args[2]).expect("cast failed");
+        let start = as_int64_array(&args[3]).expect("cast failed").iter().next().unwrap_or(None);
+        let n = as_int64_array(&args[4]).expect("cast failed").iter().next().unwrap_or(None);
+    
+        let flag_string = if args.len() == 6 {
+            let flags_array = as_string_array(&args[5]).expect("cast failed");
+            if let Some(flag) = flags_array.iter().next().unwrap_or(None) {
+                format!("(?{})", flag)
+            } else {
+                String::new()
+            }
+        } else {
+            String::new()
+        };        
+    
+        let array = input.into_iter().zip(pattern.into_iter().zip(replacement.into_iter())).map(|(text, (pat, rep))| {
+            if let (Some(text), Some(pat), Some(rep)) = (text, pat, rep) {
+                let prefix = &text[..start.unwrap_or(0) as usize];
+                let rest_text = &text[start.unwrap_or(0) as usize..];
+                let re = Regex::new(&format!("{}{}", flag_string, pat)).expect("Invalid regex pattern");
+                
+                if n == Some(0) {
+                    // 替换所有匹配
+                    Some(format!("{}{}", prefix, re.replace_all(rest_text, rep)))
+                } else {
+                    let mut replaced_text = String::from(prefix);
+                    let mut count = 0;
+                    let mut last_end = 0;
+                    for cap in re.captures_iter(rest_text) {
+                        count += 1;
+                        replaced_text.push_str(&rest_text[last_end..cap.get(0).unwrap().start()]);
+                        if count == n.unwrap() {
+                            replaced_text.push_str(rep);
+                        } else {
+                            replaced_text.push_str(&cap.get(0).unwrap().as_str());
+                        }
+                        last_end = cap.get(0).unwrap().end();
+                    }
+                    replaced_text.push_str(&rest_text[last_end..]);
+                    Some(replaced_text)
+                }
+            } else {
+                None
+            }
+        }).collect::<StringArray>();        
+        
+        Ok(Arc::new(array) as ArrayRef)
+    }    
+}
+
 // Function package declaration
 pub struct FunctionPackage;
 
 impl ScalarFunctionPackage for FunctionPackage {
     fn functions(&self) -> Vec<Box<dyn ScalarFunctionDef>> {
-        vec![Box::new(RegexpCountFunction), Box::new(RegexpLikeFunction)]
+        vec![Box::new(RegexpCountFunction), Box::new(RegexpLikeFunction),Box::new(RegexpReplaceFunction)]
     }
 }
 
@@ -183,5 +261,21 @@ mod test {
         Ok(())
     }        
 
-    
+    #[tokio::test]
+    async fn test_regexp_replace() -> Result<()> {
+        // 测试第二个匹配的'.'替换为'X'
+        //test_expression!("regexp_replace('Thomas', '\\.', 'X', 3, 2)", "ThoXas");
+
+        // 测试从第2个字符开始, 替换所有的'a'为'X'
+        test_expression!("regexp_replace('banana', 'a', 'X', 1, 0)", "bXnXnX");
+
+        // 测试从第2个字符开始, 替换第1个匹配的'a'为'X'
+        test_expression!("regexp_replace('banana', 'a', 'X', 2, 1)", "bXnana");
+
+        // 测试不考虑大小写，从第2个字符开始, 替换第1个匹配的'a'或'A'为'X'
+        test_expression!("regexp_replace('bAnana', 'a', 'X', 2, 1, 'i')", "bXnana");
+
+        Ok(())
+    }
+
 }
