5f9adf47489f94d7dda28feed87953381230df61
lbroot <l0broot@gmail.com>
2023-11-06 17:12:35 +0800

    format's test_expression

--- extension/functions/src/postgres.rs
+++ extension/functions/src/postgres.rs
@@ -14,12 +14,12 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
- 
+
 use arrow::array::{ArrayRef, StringArray};
 use arrow::datatypes::DataType;
 use datafusion::error::Result;
 use datafusion::logical_expr::Volatility;
-use datafusion_common::cast::{as_string_array};
+use datafusion_common::cast::as_string_array;
 use datafusion_expr::{
     ReturnTypeFunction, ScalarFunctionDef, ScalarFunctionPackage, Signature,
 };
@@ -44,29 +44,35 @@ impl ScalarFunctionDef for FormatFunction {
 
     fn execute(&self, args: &[ArrayRef]) -> Result<ArrayRef> {
         assert!(args.len() >= 1);
-        
-        let format_array = as_string_array(&args[0]).expect("cast failed");
-        let args_array: Vec<_> = args[1..].iter().map(|arg| as_string_array(arg).expect("cast failed")).collect();
 
-        let result_array: Vec<Option<String>> = format_array.into_iter().enumerate().map(|(idx, format)| {
-            if let Some(format) = format {
-                let mut result = format.to_owned();
-                for (i, arg_array) in args_array.iter().enumerate() {
-                    let replace_str = format!("%{}$s", i + 1);
-                    let replace_with = arg_array.value(idx);
-                    // Simply replacing the contents of result
-                    result = result.replace(&replace_str, &replace_with);
-                }                
-                Some(result)
-            } else {
-                None
-            }
-        }).collect();
+        let format_array = as_string_array(&args[0]).expect("cast failed");
+        let args_array: Vec<_> = args[1..]
+            .iter()
+            .map(|arg| as_string_array(arg).expect("cast failed"))
+            .collect();
+
+        let result_array: Vec<Option<String>> = format_array
+            .into_iter()
+            .enumerate()
+            .map(|(idx, format)| {
+                if let Some(format) = format {
+                    let mut result = format.to_owned();
+                    for (i, arg_array) in args_array.iter().enumerate() {
+                        let replace_str = format!("%{}$s", i + 1);
+                        let replace_with = arg_array.value(idx);
+                        // Simply replacing the contents of result
+                        result = result.replace(&replace_str, &replace_with);
+                    }
+                    Some(result)
+                } else {
+                    None
+                }
+            })
+            .collect();
 
         Ok(Arc::new(StringArray::from(result_array)) as ArrayRef)
     }
 }
-
 
 // Function package declaration
 pub struct FunctionPackage;
@@ -90,8 +96,49 @@ mod test {
     #[tokio::test]
     async fn test_format() -> Result<()> {
         test_expression!("format('Hello %s, %1$s', 'World')", "'Hello World, World'");
-        // Add more test cases as needed.
-        Ok(())
-    }    
 
+        // Test using different argument positions
+        test_expression!("format('%2$s %1$s', 'World', 'Hello')", "'Hello World'");
+
+        // Test strings that include escape characters
+        test_expression!("format('%s', E'O\\'Reilly')", "'O''Reilly'");
+
+        // Test integer formatting
+        test_expression!("format('Number: %d', 42)", "'Number: 42'");
+
+        // Test floating-point number formatting
+        test_expression!("format('Float: %f', 3.1415)", "'Float: 3.141500'");
+
+        // Test left-aligned padding width
+        test_expression!("format('|%-10s|', 'left')", "'|left      |'");
+
+        // Test right-aligned padding width
+        test_expression!("format('|%10s|', 'right')", "'|     right|'");
+
+        // Test dynamically specifying width
+        test_expression!("format('|%*s|', 10, 'width')", "'|     width|'");
+
+        // Test using width parameter and negative value
+        test_expression!("format('|%*s|', -10, 'neg')", "'|neg       |'");
+
+        // Test formatting of multiple mixed types
+        test_expression!(
+            "format('ID: %d, Amount: $%.2f', 7, 300.5)",
+            "'ID: 7, Amount: $300.50'"
+        );
+
+        // Test mixing position and non-position format specifiers
+        test_expression!(
+            "format('%2$s is %1$d years old.', 30, 'Alice')",
+            "'Alice is 30 years old.'"
+        );
+
+        // Test output of the special character %%
+        test_expression!("format('%% Complete')", "'% Complete'");
+
+        // Test handling of NULL values
+        test_expression!("format('This is %s', NULL)", "'This is NULL'");
+
+        Ok(())
+    }
 }
