d9ea7294f7b2ed5c6f607b0e36c20bfbf2d04bac
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-07-18 11:27:34 +0200

    Implement equals for stateful functions (#16781)
    * Implement equals for stateful functions
    
    Default implementation of `ScalarUDFImpl::equals`,
    `AggregateUDFImpl::equals` and `WindowUDFImpl::equals` is correct for
    stateless functions and those which only state is the `Signature`, which
    is most of the functions.
    
    This implements `equals` and `hash_value` for functions that have state
    other than `Signature` object.
    
    This fixes correctness issues which could occur when such stateful
    functions are used together in one query.
    
    * downgrade for MSRV
    
    * Improve doc
    
    * Update default UDF:: equals to compare aliases too
    
    * Update default UDF:: equals to compare type too (‼️)
    
    * remove now-obsoleted UDF equals/hash customizations
    
    remove these which compare signature, aliases, as the default handles
    these now
    
    * remove equals impl which compares name, signature -- default does that
    
    * cleanup imports
    
    (cherry picked from commit afd82351ca09e3320be847211ad0f246edfc0eb3)

--- datafusion/functions-window/src/nth_value.rs
+++ datafusion/functions-window/src/nth_value.rs
@@ -22,6 +22,7 @@ use crate::utils::{get_scalar_value_from_args, get_signed_integer};
 use std::any::Any;
 use std::cmp::Ordering;
 use std::fmt::Debug;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use std::ops::Range;
 use std::sync::OnceLock;
 
@@ -76,7 +77,7 @@ pub fn nth_value(arg: datafusion_expr::Expr, n: i64) -> datafusion_expr::Expr {
 }
 
 /// Tag to differentiate special use cases of the NTH_VALUE built-in window function.
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum NthValueKind {
     First,
     Last,
@@ -257,6 +258,23 @@ impl WindowUDFImpl for NthValue {
             NthValueKind::Last => Some(get_last_value_doc()),
             NthValueKind::Nth => Some(get_nth_value_doc()),
         }
+    }
+
+    fn equals(&self, other: &dyn WindowUDFImpl) -> bool {
+        let Some(other) = other.as_any().downcast_ref::<Self>() else {
+            return false;
+        };
+        let Self { signature, kind } = self;
+        signature == &other.signature && kind == &other.kind
+    }
+
+    fn hash_value(&self) -> u64 {
+        let Self { signature, kind } = self;
+        let mut hasher = DefaultHasher::new();
+        std::any::type_name::<Self>().hash(&mut hasher);
+        signature.hash(&mut hasher);
+        kind.hash(&mut hasher);
+        hasher.finish()
     }
 }
 
